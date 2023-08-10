8aff5683dc7de7c30410920a83d98e40a396773d
NeilXenon <nilamadhab007@gmail.com>
2023-08-11 04:06:22 +0530

    added animation for subject entry

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -4,8 +4,119 @@ import 'package:chime/config/colorscheme.dart';
 import 'package:flutter/material.dart';
 import 'package:simple_shadow/simple_shadow.dart';
 
-List<Widget> bottomNavigation(BuildContext context) {
-  return [];
+class SubjectEntryComponent extends StatelessWidget {
+  final int index;
+  final void Function() removeEntry;
+
+  const SubjectEntryComponent({
+    super.key,
+    required this.index,
+    required this.removeEntry,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 13),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            "class$index:",
+            style: const TextStyle(
+              fontFamily: "Poppins",
+              fontSize: 14,
+            ),
+          ),
+          Container(
+            width: 100,
+            height: 40,
+            decoration: BoxDecoration(
+              border: Border.all(
+                width: 1.1,
+                color: Colors.grey,
+              ),
+              borderRadius: const BorderRadius.all(
+                Radius.circular(10),
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const Icon(
+                  Icons.calendar_month,
+                  color: Colors.grey,
+                  size: 16,
+                ),
+                const Text(
+                  "Day",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontFamily: "Poppins",
+                    fontSize: 14,
+                  ),
+                ),
+                Container(
+                  width: 5,
+                ),
+                const Icon(
+                  Icons.keyboard_arrow_down,
+                  color: Colors.grey,
+                  size: 16,
+                )
+              ],
+            ),
+          ),
+          Container(
+            width: 100,
+            height: 40,
+            decoration: BoxDecoration(
+              border: Border.all(
+                width: 1.1,
+                color: Colors.grey,
+              ),
+              borderRadius: const BorderRadius.all(
+                Radius.circular(10),
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const Icon(
+                  Icons.timer,
+                  color: Colors.grey,
+                  size: 16,
+                ),
+                const Text(
+                  "Time",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontFamily: "Poppins",
+                    fontSize: 14,
+                  ),
+                ),
+                Container(
+                  width: 5,
+                ),
+                const Icon(
+                  Icons.keyboard_arrow_down,
+                  color: Colors.grey,
+                  size: 16,
+                )
+              ],
+            ),
+          ),
+          IconButton(
+            onPressed: removeEntry,
+            icon: const Icon(
+              Icons.delete,
+              color: Colors.grey,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
 
 class SubjectInputComponent extends StatelessWidget {
@@ -16,47 +127,51 @@ class SubjectInputComponent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 13),
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width * 0.9 * 0.9,
-        height: 40,
-        child: TextFormField(
-          onTapOutside: (event) {
-            FocusScope.of(context).unfocus();
-          },
-          decoration: InputDecoration(
-            hintText: hintText,
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(15.0),
-              borderSide: const BorderSide(
-                color: Colors.grey,
-                width: 1,
+      child: Column(
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.9 * 0.9,
+            height: 40,
+            child: TextFormField(
+              onTapOutside: (event) {
+                FocusScope.of(context).unfocus();
+              },
+              decoration: InputDecoration(
+                hintText: hintText,
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                  borderSide: const BorderSide(
+                    color: Colors.grey,
+                    width: 1,
+                  ),
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(15.0),
+                  ),
+                  borderSide: BorderSide(
+                    color: Colors.grey,
+                  ),
+                ),
+                filled: true,
+                fillColor: Colors.white,
+                contentPadding: const EdgeInsets.symmetric(
+                  vertical: 5.0,
+                  horizontal: 15,
+                ),
+                hintStyle: const TextStyle(
+                  color: Colors.grey,
+                  fontFamily: "Poppins",
+                  fontSize: 16,
+                ),
               ),
-            ),
-            focusedBorder: const OutlineInputBorder(
-              borderRadius: BorderRadius.all(
-                Radius.circular(15.0),
+              style: const TextStyle(
+                fontSize: 15.0,
+                color: Colors.black,
               ),
-              borderSide: BorderSide(
-                color: Colors.grey,
-              ),
-            ),
-            filled: true,
-            fillColor: Colors.white,
-            contentPadding: const EdgeInsets.symmetric(
-              vertical: 5.0,
-              horizontal: 15,
-            ),
-            hintStyle: const TextStyle(
-              color: Colors.grey,
-              fontFamily: "Poppins",
-              fontSize: 16,
             ),
           ),
-          style: const TextStyle(
-            fontSize: 15.0,
-            color: Colors.black,
-          ),
-        ),
+        ],
       ),
     );
   }
@@ -68,6 +183,16 @@ class Dashboard extends StatefulWidget {
 
   @override
   State<Dashboard> createState() => _DashboardState();
+}
+
+class SubjectEntryData {
+  String day;
+  String time;
+
+  SubjectEntryData({
+    required this.day,
+    required this.time,
+  });
 }
 
 class _DashboardState extends State<Dashboard>
@@ -88,6 +213,20 @@ class _DashboardState extends State<Dashboard>
       curve: Curves.fastOutSlowIn,
     ),
   );
+
+  late final Animation<double> _slideTransition = Tween<double>(
+    begin: -300,
+    end: 110,
+  ).animate(
+    CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.fastOutSlowIn,
+    ),
+  );
+
+  List<SubjectEntryData> subjectsEntries = [
+    SubjectEntryData(day: "", time: ""),
+  ];
 
   @override
   void initState() {
@@ -110,17 +249,17 @@ class _DashboardState extends State<Dashboard>
               height: MediaQuery.of(context).size.height - 100 - 70,
               color: const Color(0xffEFF1FF),
               //render all subjects in here
-              // child: const Center(
-              //   child: Text(
-              //     "Tap the + button  to add a subject to the dashboard",
-              //     textAlign: TextAlign.center,
-              //     style: TextStyle(
-              //       fontFamily: "Roboto",
-              //       fontSize: 20,
-              //       color: Color(0xff8E8E8E),
-              //     ),
-              //   ),
-              // ),
+              child: const Center(
+                child: Text(
+                  "Tap the + button  to add a subject to the dashboard",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: "Roboto",
+                    fontSize: 20,
+                    color: Color(0xff8E8E8E),
+                  ),
+                ),
+              ),
             ),
           ),
           Positioned(
@@ -139,7 +278,9 @@ class _DashboardState extends State<Dashboard>
             ),
           ),
           Positioned(
-            bottom: MediaQuery.of(context).viewInsets.bottom > 0 ? 10 : 110,
+            bottom: MediaQuery.of(context).viewInsets.bottom > 0
+                ? 10
+                : _slideTransition.value,
             child: SimpleShadow(
               opacity: 0.2, // Default: 0.5
               offset: const Offset(0, 3), // Default: Offset(2, 2)
@@ -149,134 +290,56 @@ class _DashboardState extends State<Dashboard>
                 child: Center(
                   child: Container(
                     width: MediaQuery.of(context).size.width * 0.9,
-                    height: 230,
+                    height: 240,
                     decoration: const BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.all(
                         Radius.circular(20),
                       ),
                     ),
-                    child: Column(
+                    child: ListView(
+                      padding: const EdgeInsets.all(0),
                       children: [
-                        Container(
-                          height: 12,
-                        ),
                         const SubjectInputComponent(hintText: "Subject name"),
                         const SubjectInputComponent(
                           hintText: "Professor's name",
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 13),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              const Text(
-                                "class1:",
-                                style: TextStyle(
-                                  fontFamily: "Poppins",
-                                  fontSize: 14,
-                                ),
-                              ),
-                              Container(
-                                width: 100,
-                                height: 40,
-                                decoration: BoxDecoration(
-                                  border: Border.all(
-                                    width: 1.1,
-                                    color: Colors.grey,
-                                  ),
-                                  borderRadius: const BorderRadius.all(
-                                    Radius.circular(10),
-                                  ),
-                                ),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceAround,
-                                  children: [
-                                    const Icon(
-                                      Icons.calendar_month,
-                                      color: Colors.grey,
-                                      size: 16,
-                                    ),
-                                    const Text(
-                                      "Day",
-                                      style: TextStyle(
-                                        color: Colors.grey,
-                                        fontFamily: "Poppins",
-                                        fontSize: 14,
-                                      ),
-                                    ),
-                                    Container(
-                                      width: 5,
-                                    ),
-                                    const Icon(
-                                      Icons.keyboard_arrow_down,
-                                      color: Colors.grey,
-                                      size: 16,
-                                    )
-                                  ],
-                                ),
-                              ),
-                              Container(
-                                width: 100,
-                                height: 40,
-                                decoration: BoxDecoration(
-                                  border: Border.all(
-                                    width: 1.1,
-                                    color: Colors.grey,
-                                  ),
-                                  borderRadius: const BorderRadius.all(
-                                    Radius.circular(10),
-                                  ),
-                                ),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceAround,
-                                  children: [
-                                    const Icon(
-                                      Icons.timer,
-                                      color: Colors.grey,
-                                      size: 16,
-                                    ),
-                                    const Text(
-                                      "Time",
-                                      style: TextStyle(
-                                        color: Colors.grey,
-                                        fontFamily: "Poppins",
-                                        fontSize: 14,
-                                      ),
-                                    ),
-                                    Container(
-                                      width: 5,
-                                    ),
-                                    const Icon(
-                                      Icons.keyboard_arrow_down,
-                                      color: Colors.grey,
-                                      size: 16,
-                                    )
-                                  ],
-                                ),
-                              ),
-                              const Icon(
-                                Icons.delete,
-                                color: Colors.grey,
-                              )
-                            ],
-                          ),
-                        ),
+                        ...[
+                          for (int i = 0; i < subjectsEntries.length; i++)
+                            SubjectEntryComponent(
+                              index: i + 1,
+                              removeEntry: () => setState(() {
+                                if (subjectsEntries.length > 1) {
+                                  subjectsEntries.removeAt(i);
+                                }
+                              }),
+                            ),
+                        ],
                         Padding(
                           padding: const EdgeInsets.only(top: 5),
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              const Padding(
-                                padding: EdgeInsets.only(left: 10),
-                                child: Text(
-                                  "add more classes +",
-                                  style: TextStyle(
-                                    color: chimePurple,
-                                    fontFamily: "Poppins",
-                                    fontSize: 14,
+                              Padding(
+                                padding: const EdgeInsets.only(left: 10),
+                                child: TextButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      subjectsEntries.add(
+                                        SubjectEntryData(
+                                          day: "",
+                                          time: "",
+                                        ),
+                                      );
+                                    });
+                                  },
+                                  child: const Text(
+                                    "add more classes +",
+                                    style: TextStyle(
+                                      color: chimePurple,
+                                      fontFamily: "Poppins",
+                                      fontSize: 14,
+                                    ),
                                   ),
                                 ),
                               ),
